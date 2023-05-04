701f298492ff18e22c5ac536fcc848a6a8998f92
Yamna <yamna@gmail.com>
2023-05-04 14:30:31 +0500

    New changes in main2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Welcome to task</h1>
         <p>Hello world</p>
+        <p>there is some changes</p>
       </header>
     </div>
   );
